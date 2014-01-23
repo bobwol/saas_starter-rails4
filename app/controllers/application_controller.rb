06eb3456eadf5ce6f62ffe5f806f4895e8481568
Stuart Hannig <stuarthannig@gmail.com>
2014-01-23 07:26:53 +0000

    Add terms of service acceptance param for User sign up.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   include Pundit
   
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+  
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -16,5 +18,11 @@ private
     else
       "application"                 # Everything else
     end
+  end
+  
+protected
+  # Add custom strong params for User Sign Up
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :terms_of_service
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,7 @@ class User < ActiveRecord::Base
   has_many :subscriptions, dependent: :destroy
   has_one :current_subscription, -> { where 'end_date >= ?', Date.today }, class_name: 'Subscription'
   
-  #<div class="checkbox">
-  #  <%= f.check_box :terms_of_service %>
-  #  <%= f.label :terms_of_service, "Yes, I have read and accept the #{link_to 'terms of service', page_path('tos'), target: '_blank'} agreement.".html_safe %>
-  #</div>
-  #validates :terms_of_service, acceptance: { allow_nil: false }
+  validates :terms_of_service, acceptance: { allow_nil: false }
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
