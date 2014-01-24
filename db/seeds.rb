79ee9e43e2cdab5a21cec5b381ff48026e8ab816
Stuart Hannig <stuarthannig@gmail.com>
2014-01-24 08:05:08 +0000

    Replace deprecated methods in db:seeds file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,66 +2,74 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
 puts 'PLANS'
-plan = Plan.find_or_create_by_name 'Basic'
-plan.price = 0
-plan.features = <<-eos
+Plan.find_or_create_by(name 'Basic') do |p|
+  p.price = 0
+  p.features = <<-eos
 *5* team members
 *Digital* recurring billing
 *Virtual* online terminal
 *10* total products
 *1.0%* Transaction fee
 eos
-plan.is_featured = false
-plan.has_trial = false
-plan.save
-puts "plan: #{plan.name}"
+  p.is_featured = false
+  p.has_trial = false
+  
+  puts "plan: #{p.name}"
+end
 
-plan = Plan.find_or_create_by_name 'Advanced'
-plan.price = 5
-plan.features = <<-eos
+Plan.find_or_create_by(name 'Advanced') do |p|
+  p.price = 5
+  p.features = <<-eos
 *10* team members
 *Digital* recurring billing
 *Virtual* online terminal
 *20* total products
 *0.75%* Transaction fee
 eos
-plan.is_featured = true
-plan.has_trial = true
-plan.save
-puts "plan: #{plan.name}"
+  p.is_featured = true
+  p.has_trial = true
+  
+  puts "plan: #{p.name}"
+end
 
-plan = Plan.find_or_create_by_name 'Deluxe'
-plan.price = 10
-plan.features = <<-eos
+Plan.find_or_create_by(name 'Deluxe') do |p|
+  p.price = 10
+  p.features = <<-eos
 *15* team members
 *Digital* recurring billing
 *Virtual* online terminal
 *30* total products
 *0.5%* Transaction fee
 eos
-plan.is_featured = false
-plan.has_trial = false
-plan.save
-puts "plan: #{plan.name}"
+  p.is_featured = false
+  p.has_trial = false
+
+  puts "plan: #{p.name}"
+end
 
 puts "\nADMIN USER"
-user = User.find_or_create_by_email email: ENV['ADMIN_EMAIL'].dup, 
-  password: ENV['ADMIN_PASSWORD'].dup, 
-  password_confirmation: ENV['ADMIN_PASSWORD'].dup, 
-  first_name: ENV['ADMIN_FIRST_NAME'].dup, 
-  last_name: ENV['ADMIN_LAST_NAME'].dup,
-  is_admin: true,
-  terms_of_service: 1
-puts "admin: #{user.email}"
+User.find_or_create_by(email: ENV['ADMIN_EMAIL'].dup) do |u|
+  u.password = ENV['ADMIN_PASSWORD'].dup
+  u.password_confirmation = ENV['ADMIN_PASSWORD'].dup
+  u.first_name = ENV['ADMIN_FIRST_NAME'].dup
+  u.last_name = ENV['ADMIN_LAST_NAME'].dup
+  u.is_admin = true
+  u.terms_of_service = 1
+  
+  puts "admin: #{u.email}"
+end
 
 if Rails.env.development? == true
   puts "\nTEST USER"
-  user = User.find_or_create_by_email email: "test-#{ENV['ADMIN_EMAIL']}", 
-    password: ENV['ADMIN_PASSWORD'].dup, 
-    password_confirmation: ENV['ADMIN_PASSWORD'].dup, 
-    first_name: 'Test', 
-    last_name: 'User',
-    terms_of_service: 1
-  puts "user: #{user.email}"  
-  user.subscriptions.new(plan: Plan.first)
+  user = User.find_or_create_by(email: "test-#{ENV['ADMIN_EMAIL']}") do |u|
+    u.password = ENV['ADMIN_PASSWORD'].dup
+    u.password_confirmation = ENV['ADMIN_PASSWORD'].dup
+    u.first_name = 'Test'
+    u.last_name = 'User'
+    u.terms_of_service = 1
+    
+    u.subscriptions.new(plan: Plan.first)
+    
+    puts "user: #{u.email}"  
+  end
 end
